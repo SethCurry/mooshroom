1b95ac0c3cb20d1be7c62c788b1346a6e2e86e37
Seth Curry <seth@scurry.io>
2025-11-30 16:39:51 -0500

    cljsbuild won't work with chart.js, removing the Clojure backend

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java $JVM_OPTS -cp target/mooshroom.jar clojure.main -m mooshroom.server
--- env/prod/cljs/mooshroom/prod.cljs
+++
@@ -1,7 +0,0 @@
-(ns mooshroom.prod
-  (:require [mooshroom.core :as core]))
-
-;;ignore println statements in prod
-(set! *print-fn* (fn [& _]))
-
-(core/init!)
