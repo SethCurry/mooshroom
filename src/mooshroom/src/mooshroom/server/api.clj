47e5785e88638c793c60da6664857e00f21980e3
Seth Curry <seth@scurry.io>
2025-12-08 15:10:19 -0500

    Adding exception middleware

--- src/mooshroom/src/mooshroom/server/api.clj
+++ src/mooshroom/src/mooshroom/server/api.clj
@@ -3,6 +3,8 @@
             [cheshire.core :refer [generate-string]]
             [reitit.ring :as reitit-ring]
             [reitit.ring.middleware.parameters :refer [parameters-middleware]]
+            [reitit.ring.middleware.exception :as exception]
+            [taoensso.telemere :as t]
             [mooshroom.server.api.enclosure-api :refer [view-enclosures new-enclosure get-enclosure]]
             [mooshroom.server.api.spore-api :refer [list-spores get-spore]]))
 
@@ -15,19 +17,39 @@
      :headers {"Content-Type" "application/json"}
      :body data}))
 
+(derive ::not-found ::exception)
+
+(defn throw-not-found-exc [name]
+  (throw (ex-info (str "Resource not found: " name) {:type ::not-found})))
+
+(def exception-middleware
+  (exception/create-exception-middleware
+   (merge
+    exception/default-handlers
+    {
+     ::error (fn [message exception request]
+               (t/log! {:level :error :msg "Error in request" :data {:message message :exception exception :request request}}))
+     ::not-found (fn [message exception request]
+                   (t/log! {:level :error :msg "Resource not found" :data {:message message :exception exception :request request}})
+                   {:status 404
+                    :headers {"Content-Type" "application/json"}
+                    :body (generate-string {:error "Resource not found"})})
+     ::exception/default (fn [message exception request]
+                           (t/log! {:level :error :msg "Error in request" :data {:message message :exception exception :request request}}))
+    })))
+
 (def app
   (reitit-ring/ring-handler
    (reitit-ring/router ["/api/v1"
                         ["/spores"
-                         ["" {:get {:handler list-spores
-                                    :middleware [parameters-middleware]}}]
+                         ["" {:get {:handler list-spores}}]
                          ["/:spore-id" {:get {:handler get-spore}}]]
                         ["/dht_sensors/:dht_sensor_id/data" {:get {:handler get-dht-sensor-data}}]
                         ["/enclosures"
                          ["" {:get {:handler view-enclosures}
                               :post {:handler new-enclosure}}]
-                         ["/:enclosure-id" {:get {:handler get-enclosure}}]]])
+                         ["/:enclosure-id" {:get {:handler get-enclosure}}]]]
+                       {:data {:middleware [parameters-middleware exception-middleware]}})
    (reitit-ring/routes
     (reitit-ring/create-resource-handler {:path "/" :root "/public"})
     (reitit-ring/create-default-handler))))
-
